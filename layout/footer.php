b7661b854232f9c8882421fc062fb4929435d518
dulghani <alghani183@gmail.com>
2023-01-13 16:48:18 +0700

    INPUT LINE

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; CV. MITRA 2020</span>
+            <span>Copyright &copy; Bilqist Az-Zahra</span>
         </div>
     </div>
 </footer>
